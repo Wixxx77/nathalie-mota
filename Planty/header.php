c0ef506874ba0db0367103396b05f6944fe848d4
Wixxx77 <mathisrodriguez2005@gmail.com>
2025-06-23 15:16:08 +0200

    Mise a jour

---
+++ Planty/header.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Fleurs_d\'oranger_&_Chats_errants
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+<div id="page" class="site">
+<div id="menu" class="menu">    
+                <h1 class="slogant">Fleurs d'oranger &amp; chats errants</h1>
+                <div id="toggleBtn" class="button">
+                    <span class="burger-icon">
+                        <span class="bar"></span>
+                        <span class="bar"></span>
+                        <span class="bar"></span>
+                    </span>
+                </div>
+</div>
+<div id="mySidenav" class="sidenav">
+                <a id="closeBtn" href="#" class="close"></a>
+                <div class="littleLogo">
+                    <img src="https://koukaki.olry-loic.fr/wp-content/themes/foce-child/assets/images/littleLogo.png " alt="">
+                </div>
+                <ul>
+                    <li><a href="#histoire">
+                        <span class="text">Histoire</span>
+                    </a></li>
+                    <li><a href="#personnages">
+                        <span class="text">Personnages</span>
+                    </a></li>
+                    <li><a href="#Lieu">
+                        <span class="text">Lieu</span>
+                    </a></li>
+                    <li><a href="#Studio Koukaki">
+                        <span class="text">Studio Koukaki</span>
+                    </a></li>
+                </ul>
+                <img src="/wp-content/uploads/2024/07/1.png " class="menuImg1" alt="">
+                <img src="/wp-content/uploads/2024/07/2.png " class="menuImg2" alt="">
+                <img src="/wp-content/uploads/2024/07/3.png " class="menuImg3" alt="">
+                <img src="/wp-content/uploads/2024/07/4.png " class="menuImg4" alt="">
+                <img src="/wp-content/uploads/2024/07/5.png " class="menuImg5" alt="">
+                <img src="/wp-content/uploads/2024/07/6.png " class="menuImg6" alt="">
+                <img src="/wp-content/uploads/2024/07/7.png " class="menuImg7" alt="">
+                <img src="/wp-content/uploads/2024/07/8.png " class="menuImg8" alt="">
+                <p class="legend">Studio Koukaki</p>
+</div>
+
+
+		</nav><!-- #site-navigation -->
+	</header><!-- #masthead -->
+
+    <!--<div class="menuImg">
+                    <img src="/wp-content/uploads/2024/07/1.png " class="Img1" alt="">
+                    <img src="/wp-content/uploads/2024/07/2.png " class="Img2" alt="">
+                    <img src="/wp-content/uploads/2024/07/3.png " class="Img3" alt="">
+                    <img src="/wp-content/uploads/2024/07/4.png " class="Img4" alt="">
+                    <img src="/wp-content/uploads/2024/07/5.png " class="Img5" alt="">
+                    <img src="/wp-content/uploads/2024/07/6.png " class="Img6" alt="">
+                    <img src="/wp-content/uploads/2024/07/7.png " class="Img7" alt="">
+                    <img src="/wp-content/uploads/2024/07/8.png " class="Img8" alt="">
+                </div>-->
